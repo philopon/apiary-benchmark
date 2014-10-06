90ecee2db08bcb6f06ff7a83e9ed74b0f59cd113
philopon <philopon.dependence@gmail.com>
2014-10-06 18:04:41 +0900

    update results.

--- scripts/aggregate.sh
+++ scripts/aggregate.sh
@@ -11,8 +11,5 @@ for DIR in `find $RESULTDIR -mindepth 1 -maxdepth 1 -type d`; do
   bases=$(for FILE in `ls */*/*.log`; do
     tail -n 1 $FILE
   done | python $SDIR/collect.py)
-  for base in $bases; do
-    gnuplot -e "ifile=\"$base.dat\";ofile=\"$base.png\"" $SDIR/plot.gnuplot
-  done
 done
 
